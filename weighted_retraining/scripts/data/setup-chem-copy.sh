8fe5bb3d19fce9964bc123dfb576ceea0e2ae133
PTAEJOOO <qkrxown5985@gmail.com>
2024-03-12 18:47:20 +0900

    0312

--- weighted_retraining/scripts/data/setup-chem-copy.sh
+++ weighted_retraining/scripts/data/setup-chem-copy.sh
@@ -10,15 +10,15 @@ logP_script="weighted_retraining/weighted_retraining/chem/calc_penalized_logP.py
 QED_script="weighted_retraining/weighted_retraining/chem/calc_qed.py"
 
 # Calculate penalized logP for all files
-all_smiles_file="$chem_dir/all.txt"
-cat "$chem_dir/train.txt" "$chem_dir/val.txt" > "$all_smiles_file"
-python "$logP_script" \
-    --input_file "$chem_dir/train.txt" "$chem_dir/val.txt" \
-    --output_file="$chem_dir/pen_logP_all.pkl"
+# all_smiles_file="$chem_dir/all.txt"
+# cat "$chem_dir/train.txt" "$chem_dir/val.txt" > "$all_smiles_file"
+# python "$logP_script" \
+#     --input_file "$chem_dir/train.txt" "$chem_dir/val.txt" \
+#     --output_file="$chem_dir/pen_logP_all.pkl"
 
-python "$QED_script" \
-    --input_file "$chem_dir/train.txt" "$chem_dir/val.txt" \
-    --output_file="$chem_dir/qed_all.pkl"
+# python "$QED_script" \
+#     --input_file "$chem_dir/train.txt" "$chem_dir/val.txt" \
+#     --output_file="$chem_dir/qed_all.pkl"
 
 # Next, we preprocess all the data (takes a VERY long time sadly...)
 
@@ -43,12 +43,12 @@ out_dir="$chem_dir"/tensors_val_tiny
 mkdir "$out_dir"
 python "$preprocess_script" -t "$chem_dir"/val.txt -d "$out_dir"  -e 1000
 
-# Training set (for pseudo)
-out_dir="$chem_dir"/tensors_train_pseudo
-mkdir "$out_dir"
-python "$preprocess_script"    -t "$chem_dir"/train.txt -d "$out_dir"     -e 2
+# # Training set (for pseudo)
+# out_dir="$chem_dir"/tensors_train_pseudo
+# mkdir "$out_dir"
+# python "$preprocess_script"    -t "$chem_dir"/train.txt -d "$out_dir"     -e 2
 
-# Validation set (also for pseudo)
-out_dir="$chem_dir"/tensors_val_pseudo
-mkdir "$out_dir"
-python "$preprocess_script" -t "$chem_dir"/val.txt -d "$out_dir"  -e 2
+# # Validation set (also for pseudo)
+# out_dir="$chem_dir"/tensors_val_pseudo
+# mkdir "$out_dir"
+# python "$preprocess_script" -t "$chem_dir"/val.txt -d "$out_dir"  -e 2
